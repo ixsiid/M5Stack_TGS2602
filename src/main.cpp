d5b87dbf86276748be68f2a14d524dd50adfe7de
unknown <tal@HA000102600.mcdiv.csg.kyocera.co.jp>
2020-10-07 16:07:37 +0900

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ esp_err_t initI2C(i2c_port_t i2c_port) {
 // 処理が142.857msecを超えたらどうする？
 TickType_t period_tick = (int)(142.857 * portTICK_PERIOD_MS) / 1000;
 
-gpio_num_t adj[3]	  = {GPIO_NUM_17, GPIO_NUM_5, GPIO_NUM_26};
+gpio_num_t adj[3]	  = {GPIO_NUM_34, GPIO_NUM_17, GPIO_NUM_5};
 uint8_t adj_values[7] = {
     0b000,  // 31k
     0b001,  // 26k
@@ -94,6 +94,8 @@ void app_main() {
 		lcd->update();
 		lcd->clear(BLACK);
 
+		int diff = 0;
+
 		while (true) {
 			// Core1はWatch Dog Timerを介入させるためにDelay挿入
 			vTaskDelay(1);
@@ -101,33 +103,38 @@ void app_main() {
 			if (now - current < period_tick) continue;
 			current = now;
 
-			_i("Mode [%d], Tick %d,", mode, current);
+			_v("Mode [%d], Tick %d,", mode, current);
 
 			if (mode >= 0b1110) {
 				// ディスプレイ更新モード
 				lcd->update();
 				lcd->clear(BLACK);
+
+				history[320 * 7 + historyIndex] = (diff + 239) / 2;
 				/* ラインを書く */
 				for (int x = 0; x < 320; x++) {
 					int t = historyIndex + x;
 					if (t >= 320) t -= 320;
 					for (int i = 0; i < 7; i++) {
 						uint8_t v = history[320 * i + t];
+
 						frameBuffer[320 * (239 - v) + x] = 0xffff;
 					}
+					frameBuffer[320 * (239 - history[320 * 7 + t]) + x] = 0xfc3f;
 				}
 
 				historyIndex++;
 				if (historyIndex >= 320) historyIndex = 0;
 
-				for (int x = 315; x < 320; x++)
-					for (int y = 0; y < 5; y++)
+				for (int x = 5; x < 10; x++)
+					for (int y = 230; y < 235; y++)
 						frameBuffer[y * 320 + x] = refresh ? GREEN : RED;
 
 				refresh = 1 - refresh;
 
 				// for(int i=0; i<7; i++) values[i] = 0;
 				mode = 0;
+				diff = 0;
 				continue;
 			}
 
@@ -135,7 +142,7 @@ void app_main() {
 				// インピーダンス変更モード
 				for (int i = 0; i < 3; i++) {
 					gpio_set_level(adj[i], (adj_values[mode >> 1] >> i) & 0b1);
-					_i("GPIO [%d]: to %d", adj[i], (adj_values[mode >> 1] >> i) & 0b1);
+					_v("GPIO [%d]: to %d", adj[i], (adj_values[mode >> 1] >> i) & 0b1);
 				}
 			} else {
 				// 読み取りモード
@@ -145,14 +152,20 @@ void app_main() {
 					sprintf(buf, "%d: Error", t);
 				else
 					sprintf(buf, "%d: %5d", t, values[t]);
+
 				lcd->drawString(10, 5 + 14 * t, GREEN, buf);
 
-				uint16_t v = (values[t] + 0b100000000000) >> 4;  // 12bitでサンプリングしたデータを8bitに丸める
+				uint16_t v = ((values[t] >> 8) + 0b10000000) & 0xff; // 16bitでサンプリングしたデータを8bitに丸める
 				if (v >= 240) v = 239;
 				history[320 * t + historyIndex] = v;
+
+				sprintf(buf, "(%d)", v);
+				lcd->drawString(100, 5 + 14 * t, GREEN, buf);
+
+				int prev = history[320 * t + (historyIndex <= 0 ? 319 : historyIndex - 1)];
+				if (v - prev > diff) diff = v - prev;
 			}
 			mode += 1;
 		}
-	},
-					    "TGS", 4096, lcd, 1, NULL, 1);
+	}, "TGS", 8192, lcd, 1, NULL, 1);
 }
